eab8c17713632db3ba68c0ee9e9066b2e79efc23
Sudip Jha <>
2025-03-08 11:19:00 +0530

    Class - 1 Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    
+    const header1 = React.createElement("h1", {
+        id: "header1"
+    },
+    "Namaste React");
+    const header2 = React.createElement("h2", {
+        id: "header2"
+    },
+    "Namaste React");
+    const container = React.createElement("div",{
+        id: "container1"
+    },
+    [header1, header2]);
+    root.render(container);
